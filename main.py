0856c021eaddf3ee3a921970f936cc6eac4ea747
Don Paul <donpaulpm001@gmail.com>
2023-02-28 14:35:28 +0000

    Neo4j setup

--- main.py
+++ main.py
@@ -1,14 +1,44 @@
+import os
 from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
+from neo4j import GraphDatabase
+from dotenv import load_dotenv
 
 app = FastAPI()
 
+load_dotenv()
+
+uri = os.getenv("uri")
+user = os.getenv("user")
+pwd = os.getenv("pwd")
+
+print(uri,user,pwd)
+class LogDetails(BaseModel):
+    userName: str
+    password: str
+
+
+def connection():
+    driver = GraphDatabase.driver(uri=uri, auth=(user,pwd))
+    return driver
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+@app.post("/login")
+def login_request(user_details:LogDetails):
+    return user_details
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/count")
+def get_node_count(name):
+    driver_neo4j = connection()
+    session = driver_neo4j.session()
+    q1 = """
+    match(s:Shop), (u:User) where u.name=$a return u
+    """
+    x = {"a": name}
+    result = session.run(q1, x)
+
+    return {"response":result}
